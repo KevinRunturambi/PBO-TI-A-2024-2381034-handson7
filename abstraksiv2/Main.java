b34391ce5f1dddb75738d2ba0796a320f08fd973
KevinRunturambi <2381034@unai.edu>
2024-10-31 10:47:13 +0700

    Commit

---
+++ abstraksiv2/Main.java
@@ -0,0 +1,10 @@
+
+package abstraksiv2;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print();
+        printer.scan();
+    }
+}
